0b46f85576f7af581ad4470fd5e4f7b36e27ade0
Caomeinaicha <76242204+Caomeinaicha@users.noreply.github.com>
2023-11-30 20:24:36 +0800

    Add files via upload

---
+++ Surge/Script/ipcheck.js
@@ -0,0 +1,28 @@
+
+let url = "http://ip-api.com/json/?lang=zh-CN"
+
+$httpClient.get(url, function(error, response, data){
+    let jsonData = JSON.parse(data)
+    let ip = jsonData.query
+    let country = jsonData.country
+    let emoji = getFlagEmoji(jsonData.countryCode)
+    let city = jsonData.city
+    let isp = jsonData.isp
+    
+  body = {
+    title: "网络信息",
+    content: `IP信息: ${ip}\n运营商: ${isp}\nIP位置: ${emoji}${country} - ${city}`,
+    icon: "link.icloud",
+    'icon-color': "#5AC8FA"
+  }
+  $done(body);
+});
+
+
+function getFlagEmoji(countryCode) {
+    const codePoints = countryCode
+      .toUpperCase()
+      .split('')
+      .map(char =>  127397 + char.charCodeAt());
+    return String.fromCodePoint(...codePoints);
+}
